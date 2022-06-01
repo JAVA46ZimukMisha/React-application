e05b0dd618adb92975c0ab7bd286feeaea7f9bd3
MishaZimuk <mishazimuk@gmail.com>
2022-06-01 22:57:11 +0300

    hw47

--- src/components/dialogs/Spiner.tsx
+++ src/components/dialogs/Spiner.tsx
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
 const Spinner: React.FC = () => {
-  return <Box sx={{ display: 'flex' }}>
+  return <Box sx={{textAlign: "center"}}>
              <CircularProgress />
         </Box>
 }
